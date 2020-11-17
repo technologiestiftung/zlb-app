2c854df65f351dc1f270d33061b89d55d9d7b69d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-11-17 12:52:09 +0100

    feat(ZLB1-169): highlight active card

--- source/js/overview.js
+++ source/js/overview.js
@@ -1,14 +1,14 @@
-const cardsWithSubservices = document.querySelectorAll(
+const serviceWithSubservices = document.querySelectorAll(
   ".service-overview a:not([href])"
 );
 
-const serviceGroups = Array.from(cardsWithSubservices).map((card) =>
+const serviceGroups = Array.from(serviceWithSubservices).map((card) =>
   card.getAttribute("data-service-group")
 );
 
 let activeServiceGroup = "";
 
-cardsWithSubservices.forEach((card) => {
+serviceWithSubservices.forEach((card) => {
   card.addEventListener("click", () => {
     const id = card.getAttribute("data-service-group");
 
@@ -20,16 +20,33 @@ cardsWithSubservices.forEach((card) => {
         el.style.display = "none";
       });
 
+      const deactivatedCardElement = document.querySelector(
+        `.service-overview a[data-service-group="${id}"] .card`
+      );
+      deactivatedCardElement.classList.remove("is-active");
+
       activeServiceGroup = "";
     } else {
       const activeGroupElement = document.querySelector(`#subservices-${id}`);
       activeGroupElement.style.display = "block";
+
+      const activeCardElement = document.querySelector(
+        `.service-overview a[data-service-group="${id}"] .card`
+      );
+      activeCardElement.classList.add("is-active");
 
       const inactiveGroupElements = document.querySelectorAll(
         `.service-overview .subservices-wrapper:not(#subservices-${id})`
       );
       inactiveGroupElements.forEach((el) => {
         el.style.display = "none";
+      });
+
+      const deactivatedCardElements = document.querySelectorAll(
+        `.service-overview a:not([data-service-group="${id}"]) .card`
+      );
+      deactivatedCardElements.forEach((el) => {
+        el.classList.remove("is-active");
       });
 
       activeServiceGroup = id;
