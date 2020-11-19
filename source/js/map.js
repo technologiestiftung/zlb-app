485cee3090855cd9b747d9616577336f84d04ba7
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-11-19 11:55:31 +0100

    feat(ZLB1-57): filter offices for service availability

--- source/js/map.js
+++ source/js/map.js
@@ -4,6 +4,11 @@ import mapboxgl from "mapbox-gl";
 // -------------------------------------------------------
 mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
 
+const url = new URL(window.location.href);
+const serviceId = url.searchParams.has("serviceId")
+  ? url.searchParams.get("serviceId")
+  : undefined;
+
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/light-v10",
@@ -15,34 +20,40 @@ const map = new mapboxgl.Map({
 // Set marker data
 // -------------------------------------------------------
 const fetchData = async () => {
-  const response = await fetch("/data/aemter.geojson")
-  const data = response.json();
-  return data;
-}
+  const response = await fetch("/data/aemter.geojson");
+  const data = await response.json();
+
+  // filter offices that offer selected service
+  const filtered = data.features.filter((feature) => {
+    return feature.properties.details.services.some((service) => {
+      return service.id === serviceId && service.appointment === true;
+    });
+  });
+  return {
+    ...data,
+    features: serviceId ? filtered : data.features,
+  };
+};
 
 map.on("load", () => {
   fetchData()
-    .then(data => {
+    .then((data) => {
       addMarkers(data);
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 });
 
 // -------------------------------------------------------
 // Add markers
 // -------------------------------------------------------
-const addMarkers = data => {
-
-  data.features.forEach(feature => {
-
+const addMarkers = (data) => {
+  data.features.forEach((feature) => {
     const popupContent = `
-      <p class="is-size-6 mb-0">${feature.properties.parent}</p>
-      <h1 class="title is-6 mt-1 mb-0">${feature.properties.label}</h1>
-      <p class="is-size-6 mt-1">[Straße und Hausnummer]</p>
-      <p class="is-size-6 mt-1">[PLZ und Ort]</p>
-      <div class="buttons mt-6">
-        <a class="button" disabled>Zum Standort</a>
-        <a class="button" disabled>Zur Terminvereinbarung</a>
+      <div class="px-2 py-2">
+        <p class="is-size-6 mb-0">${feature.properties.parent}</p>
+        <h1 class="title is-6 mt-1 mb-0">${feature.properties.label}</h1>
+        <p class="is-size-6 mt-1">[Straße und Hausnummer]</p>
+        <p class="is-size-6 mt-1">[PLZ und Ort]</p>
       </div>
     `;
 
@@ -50,13 +61,15 @@ const addMarkers = data => {
     markerElement.className = "marker";
 
     const popupOptions = {
-      maxWidth: "348px"
-    }
+      maxWidth: "348px",
+    };
 
     new mapboxgl.Marker(markerElement)
-      .setLngLat([feature.geometry.coordinates[0], feature.geometry.coordinates[1]])
+      .setLngLat([
+        feature.geometry.coordinates[0],
+        feature.geometry.coordinates[1],
+      ])
       .setPopup(new mapboxgl.Popup(popupOptions).setHTML(popupContent))
       .addTo(map);
-  })
-}
-
+  });
+};
